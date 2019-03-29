4c5b83362922150395f2b585890bfe65dd73bfb9
guangfu zhang <985663607@qq.com>
2019-03-29 17:46:29 +0800

    第二个版本

---
+++ Top30/Common/BaseClass/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Common.BaseClass
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+        }
+            
+    }
+}
